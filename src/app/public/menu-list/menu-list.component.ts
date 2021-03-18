e4308bd823263684e4419b436b362a646163429e
adn974 <56817528+adnan974@users.noreply.github.com>
2021-03-18 15:00:32 +0400

    feature: ajout du service des menus pour le CRUD

--- src/app/public/menu-list/menu-list.component.ts
+++ src/app/public/menu-list/menu-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'src/app/core/services/menu.service';
 import { Menu } from 'src/app/shared/models/menu';
 
 @Component({
@@ -11,18 +12,18 @@ export class MenuListComponent implements OnInit {
   public menuList:Array<Menu>;
 
 
-  constructor() { }
+  constructor(private menuService:MenuService) { }
 
   ngOnInit(): void {
-    this.menuList= new Array<Menu>()
+    this.menuList= this.menuService.getMenus();
 
-    for (let i = 1; i < 5; i++) {
-      let menu = new Menu();
-      menu.id = i;
-      menu.name = "Menu " + i;
-      menu.price = i+i;
-      this.menuList.push(menu);
-    }
+    // for (let i = 1; i < 5; i++) {
+    //   let menu = new Menu();
+    //   menu.id = i;
+    //   menu.name = "Menu " + i;
+    //   menu.price = i+i;
+    //   this.menuList.push(menu);
+    // }
   }
 
 }
