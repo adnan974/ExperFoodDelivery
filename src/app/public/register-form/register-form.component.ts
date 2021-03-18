c9ef1a71f63a5b29c636962d7f8805e696d0bcc2
mumu974 <ti.zoizo@hotmail.fr>
2021-03-18 09:17:52 +0400

    création d'un user via le formulaire d'inscription

--- src/app/public/register-form/register-form.component.ts
+++ src/app/public/register-form/register-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../shared/models/user';
 
 @Component({
   selector: 'efd-register-form',
@@ -12,6 +13,8 @@ export class RegisterFormComponent implements OnInit {
   registerLoginForm: FormGroup;
   registerInfoGroup: FormGroup;
   isEditable = true;
+  user: User;
+  hide = true;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -27,13 +30,23 @@ export class RegisterFormComponent implements OnInit {
       cpCtrl: ['', Validators.required]
 
     });
+    this.user = new User();
   }
   registerLogin() {
-
+    console.log(this.registerLoginForm.value);
+    this.user.email = this.registerLoginForm.value['emailCtrl'];
+    this.user.password = this.registerLoginForm.value['passwordCtrl'];
+    console.log(this.user);
   }
 
   register() {
-
+    console.log(this.registerInfoGroup.value);
+    this.user.lastName = this.registerInfoGroup.value['lastNameCtrl'];
+    this.user.firstName = this.registerInfoGroup.value['firstNameCtrl'];
+    this.user.address = this.registerInfoGroup.value['addressCtrl'];
+    this.user.city = this.registerInfoGroup.value['cityCtrl'];
+    this.user.CP = this.registerInfoGroup.value['cpCtrl'];
+    console.log(this.user);
   }
 
 }
