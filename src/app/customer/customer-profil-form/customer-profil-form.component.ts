98c35565910ebf528916342dbfb3c03c8525dc57
mumu974 <ti.zoizo@hotmail.fr>
2021-04-03 13:03:45 +0000

    réagencement du code et update sur le profil custumer

--- src/app/customer/customer-profil-form/customer-profil-form.component.ts
+++ src/app/customer/customer-profil-form/customer-profil-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { CustomerService } from '../../core/services/customer.service';
 import { User } from '../../shared/models/user';
 
@@ -13,27 +15,58 @@ export class CustomerProfilFormComponent implements OnInit {
   updateLoginForm?: FormGroup;
   updateInfoGroup?: FormGroup;
   isEditable = true;
-  user?: User;
   hide = true;
-  constructor(private route: Router, private fb: FormBuilder, private us: CustomerService) { }
+  @Input() user: User | undefined;
+  @Input() userconnect?: Observable<User>;
+  @Output() updateUser: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private route: Router, private fb: FormBuilder) {
+    this.updateLoginForm = this.fb.group({
+      'emailCtrl': [],
+      'passwordCtrl': [],
+    });
+    this.updateInfoGroup = this.fb.group({
+      'lastnameCtrl': [],
+        'firstnameCtrl': [],
+        'addressCtrl': [],
+        'cityCtrl': [],
+        'cpCtrl': [],
+    });
+  }
 
   ngOnInit(): void {
-    const connexionUserObject = JSON.parse(localStorage.getItem('user') ?? "");
-    const user = this.us.getCustomer(connexionUserObject._id);
+    this.userconnect?.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    )
+      .subscribe((value) => {
+        this.updateLoginForm?.get('emailCtrl')?.setValue(value.email);
+        this.updateLoginForm?.get('passwordCtrl')?.setValue(value.password);
+
+        this.updateInfoGroup?.get('lastnameCtrl')?.setValue(value.lastname);
+        this.updateInfoGroup?.get('firstnameCtrl')?.setValue(value.firstname);
+        this.updateInfoGroup?.get('addressCtrl')?.setValue(value.address);
+        this.updateInfoGroup?.get('cityCtrl')?.setValue(value.city);
+        this.updateInfoGroup?.get('cpCtrl')?.setValue(value.CP);
+      })
   }
+
   updateLogin() {
     this.user && this.updateLoginForm &&
     (this.user.email = this.updateLoginForm.value['emailCtrl']) &&
-    (this.user.password = this.updateLoginForm.value['passwordCtrl']);
-
+      (this.user.password = this.updateLoginForm.value['passwordCtrl']);
+    console.log(this.user?.email);
   }
 
   updateProfil() {
     this.user && this.updateInfoGroup &&
-    (this.user.lastname = this.updateInfoGroup.value['lastnameCtrl'])&&
-    (this.user.firstname = this.updateInfoGroup.value['firstnameCtrl'])&&
-    (this.user.address = this.updateInfoGroup.value['addressCtrl'])&&
-    (this.user.city = this.updateInfoGroup.value['cityCtrl'])&&
-    (this.user.CP = this.updateInfoGroup.value['cpCtrl'])
+      (this.user.lastname = this.updateInfoGroup.value['lastnameCtrl']) &&
+      (this.user.firstname = this.updateInfoGroup.value['firstnameCtrl']) &&
+      (this.user.address = this.updateInfoGroup.value['addressCtrl']) &&
+      (this.user.city = this.updateInfoGroup.value['cityCtrl']) &&
+      (this.user.CP = this.updateInfoGroup.value['cpCtrl']);
+    console.log(this.user);
+    this.updateUser.emit(this.user);
   }
+
 }
--- src/app/customer/customer-profil-show/customer-profil-show.component.ts
+++ src/app/customer/customer-profil-show/customer-profil-show.component.ts
@@ -1,8 +1,5 @@
-import { AuthService } from './../../core/services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonService } from '../../core/services/common.service';
-import { CustomerService } from '../../core/services/customer.service';
 import { User } from '../../shared/models/user';
 
 @Component({
@@ -12,35 +9,12 @@ import { User } from '../../shared/models/user';
 })
 export class CustomerProfilShowComponent implements OnInit {
 
-  //userConnected?: User | null;
-  user: User = new User();
-  constructor(private authService: AuthService, private userservice: CustomerService, private route: Router, private commonService: CommonService) { }
+  @Input() user: User | undefined;
+  
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
 
-    this.authService.$userConnected.subscribe((user) => {
-
-      if (user) {
-        this.userservice.getCustomer(user.id).toPromise()
-          .then((response) => {
-            this.user.firstname = response.firstname;
-            this.user.lastname = response.lastname;
-            this.user.email = response.email;
-            this.user.address = response.address;
-            this.user.CP = response.CP;
-            this.user.city = response.city;
-          }
-
-          )
-          .catch((error) => {
-            console.error(error);
-            this.commonService.changeSnackBarMessage(`Erreur : ${error.error ? error.error.message : error.message ? error.message : error}`)
-          }).finally(() => {
-
-          })
-
-      }
-    })
   }
 
   navigate(link: string) {
